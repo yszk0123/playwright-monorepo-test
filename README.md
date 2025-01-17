fc4785b8f7f8ab478de6426a847548751c1d3b89
Yuji Suzuki <y0suzuki0123@gmail.com>
2025-01-17 17:44:55 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Usage
+
+```bash
+npm install
+npm run test:e2e
+```
+
+# Directory Structure
+
+```
+packages/
+  testing/ ... test tools
+  web/ ... application code
+```
---
+++ packages/web/src/index.spec.ts
@@ -0,0 +1,35 @@
+import { test, expect } from '@playwright/test';
+
+// test('has title', async ({ page }) => {
+//   await page.goto('https://playwright.dev/');
+
+//   // Expect a title "to contain" a substring.
+//   await expect(page).toHaveTitle(/Playwright/);
+// });
+
+// test('get started link', async ({ page }) => {
+//   await page.goto('https://playwright.dev/');
+
+//   // Click the get started link.
+//   await page.getByRole('link', { name: 'Get started' }).click();
+
+//   // Expects page to have a heading with the name of Installation.
+//   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+// });
+
+test('get started link', async ({ browser }) => {
+  const context = await browser.newContext({
+    recordHar: { path: 'requests.har', mode: 'full', urlFilter: '/**/*.css' },
+  });
+  const page = await context.newPage();
+
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+
+  await context.close();
+});
